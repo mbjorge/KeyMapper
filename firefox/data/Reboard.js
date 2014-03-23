489849f9868b9cf2396e74297842cb3a471179b6
Michael Bjorge <mbjorge@student.umass.edu>
2014-03-22 21:34:42 -0400

    Created methods for adding key mappings
    Create a series of methods for creating maps from physical keys to
    emulated keys.

--- firefox/data/Reboard.js
+++ firefox/data/Reboard.js
@@ -1,34 +1,80 @@
 var Reboard = {};
 
-document.addEventListener("keydown", function (event) {
-	if (event.which == 72) {
-		triggerKeyboardEvent(document, 65);
-	} else if (event.which == 75) {
-		triggerKeyboardEvent(document, 68);
-	}
+window.addEventListener("load", function() {
+	Reboard.addKeyMapping(KeyEvent.DOM_VK_W, KeyEvent.DOM_VK_UP);
+	Reboard.addKeyMapping(KeyEvent.DOM_VK_A, KeyEvent.DOM_VK_LEFT);
+	Reboard.addKeyMapping(KeyEvent.DOM_VK_S, KeyEvent.DOM_VK_DOWN);
+	Reboard.addKeyMapping(KeyEvent.DOM_VK_D, KeyEvent.DOM_VK_RIGHT);
 });
 
 
-function triggerKeyboardEvent(el, keyCode){
+Reboard.addKeyMapping(phsyicalKeyCode, mappedKeyCode) {
+	if (typeof physicalKeyCode === "undefined" || physicalKeyCode === null) {
+		throw new TypeError("physicalKeyCode is null");
+	}
+	
+	if (typeof mappedKeyCode === "undefined" || mappedKeyCode === null) {
+		throw new TypeError("mappedKeyCode is null");
+	}
+	
+	var keyEvents = ["keyup", "keydown", "keypress"];
+	
+	keyEvents.forEach(function(keyEvent) {
+		document.addEventListener(keyEvent, function (event) {
+			if (event.keyCode === physicalKeyCode || event.which === physicalKeyCode) {
+				Reboard.triggerKeyEvent(document, keyEvent, mappedKeyCode);
+			}
+		});
+	});
+}
+
+/**
+ * @param element element to trigger the event on (usually the document object)
+ * @param keyEvent keydown, keyup, or keypress
+ */
+Reboard.triggerKeyEvent = function(element, keyEvent, keyCode){
+	if (typeof element === "undefined" || element === null) {
+		throw new TypeError("element is null");
+	}
+	
+	if (typeof keyEvent === "undefined" || keyEvent === null) {
+		throw new TypeError("keyEvent is null");
+	}
+	
+	keyEvent = keyEvent.toLowerCase();	
+	if (keyEvent !== "keydown" && keyEvent !== "keyup" && keyEvent !== "keypress") {
+		throw new TypeError("keyEvent needs to be one of 'keydown', 'keyup', or 'keypress'");
+	}
+	
+	if (typeof keyCode === "undefined" || keyCode === null) {
+		throw new TypeError("keyCode is null");
+	}	
+	
 	var keyboardEvent = document.createEvent("KeyboardEvent");
 
-	var initMethod = typeof keyboardEvent.initKeyboardEvent !== 'undefined' ? 
+	var initMethod = (typeof keyboardEvent.initKeyboardEvent !== 'undefined') ? "initKeyboardEvent" : "initKeyEvent";
 
-			"initKeyboardEvent" : "initKeyEvent";
-
-
+	var bubbles = true;
+	var cancelable = true;
+	var view = null; //could also be set to unsafeWindow, but that might not be supported in the future.
+	var ctrlKeyPressed = false;
+	var altKeyPressed = false;
+	var shiftKeyPressed = false;
+	var metaKeyPressed = false;
+	var charCode = 0; //this value is not important for Reboard's use
+	
 	keyboardEvent[initMethod](
-			"keydown",
-			true,      // bubbles oOooOOo0
-			true,      // cancelable   
-			unsafeWindow,    // view
-			false,     // ctrlKeyArg
-			false,     // altKeyArg
-			false,     // shiftKeyArg
-			false,     // metaKeyArg
+			keyEvent,
+			bubbles,
+			cancelable, 
+			view,
+			ctrlKeyPressed,
+			altKeyPressed,
+			shiftKeyPressed,
+			metaKeyPressed,
 			keyCode,  
-			0          // charCode   
+			charCode   
 	);
 
-	el.dispatchEvent(keyboardEvent); 
-}
+	element.dispatchEvent(keyboardEvent); 
+};
